3d6c70fa8fcd7c9b6190dd6e31cfa3fd40cd22f0
Yulia Lepner <ylepner@gmail.com>
2022-06-03 18:42:18 +0200

    fix: convert to ts

--- highest-and-lowest.js
+++
@@ -1,6 +0,0 @@
-function highAndLow(numbers) {
-  numbers = numbers.split(' ').map(Number)
-  return `${Math.max.apply(null, numbers)} ${Math.min.apply(null, numbers)}`
-}
-
-console.log(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"))
