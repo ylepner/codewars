20ade942dfe8f0692d64c949215bb309b6054937
Yulia Lepner <ylepner@gmail.com>
2022-06-24 23:53:17 +0200

    feat: update urban-dictionary task solution

--- urban-dictionary/urban-dictionary.ts
+++ urban-dictionary/urban-dictionary.ts
@@ -1,17 +1,15 @@
-interface WordDictionary {
-  addWord(): void,
-  search(): boolean
-}
+export class WordDictionary {
+  words: string[]
+  constructor() {
+    this.words = []
+  }
 
-const WordDictionary: any = function () {
-  this.words = []
-};
+  addWord(word: string) {
+    this.words.push(word);
+  }
 
-WordDictionary.prototype.addWord = function (word: string) {
-  this.words.push(word);
-};
-
-WordDictionary.prototype.search = function (word: string) {
-  const pattern = new RegExp(`^${word}$`);
-  return this.words.some((word: string) => pattern.test(word));
+  search(word: string) {
+    const pattern = new RegExp(`^${word}$`);
+    return this.words.some((word: string) => pattern.test(word));
+  }
 };
