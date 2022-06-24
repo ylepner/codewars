7a3b70002a037409f84a5b9623be00a4e1340965
Yulia Lepner <ylepner@gmail.com>
2022-06-24 23:53:42 +0200

    feat: add tests to urban-dictionary task

--- urban-dictionary/urban-dictionary.spec.ts
+++ urban-dictionary/urban-dictionary.spec.ts
@@ -1,13 +1,19 @@
 import { expect } from 'chai';
-import './urban-dictionary'
+import { WordDictionary } from './urban-dictionary';
 
-// describe('Urban Dictionary, 6kyu, https://www.codewars.com/kata/urban-dictionary', () => {
-//   it('case 1', () => {
-//     const wd = new WordDictionary();
-//     wd.addWord('a');
-//     wd.addWord('at');
-//     wd.addWord('ate');
-//     wd.addWord('ear');
-//     expect(wd.search('a')).to.be.eq(true);
-//   });
-// })
+describe('Urban Dictionary, 6kyu, https://www.codewars.com/kata/urban-dictionary', () => {
+  it('case 1', () => {
+    const wd = new WordDictionary();
+    wd.addWord('a');
+    wd.addWord('at');
+    wd.addWord('ate');
+    wd.addWord('ear');
+    expect(wd.search('a')).to.be.eq(true);
+    expect(wd.search('a.')).to.be.eq(true);
+    expect(wd.search('a.e')).to.be.eq(true);
+    expect(wd.search('b')).to.be.eq(false);
+    expect(wd.search('e.')).to.be.eq(false);
+    expect(wd.search('ea.')).to.be.eq(true);
+    expect(wd.search('ea..')).to.be.eq(false);
+  });
+})
