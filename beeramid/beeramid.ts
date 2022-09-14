474d3af06899e6a27e28d250ef8cfbdd4d5fa51b
Yulia Lepner <ylepner@gmail.com>
2022-09-14 20:30:12 +0200

    refactor: correct solution

--- beeramid/beeramid.ts
+++ beeramid/beeramid.ts
@@ -1,7 +1,6 @@
 export function beeramid(bonus: number, price: number): number {
   let cans = Math.floor(bonus / price)
   let level = 0
-  let count = cans - (level * level)
   while ((level + 1) ** 2 <= cans) {
     level += 1
     cans -= (level * level)
