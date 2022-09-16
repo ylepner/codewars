44d1b7a5c199d546836e11dba8dea0cf6459957b
Yulia Lepner <ylepner@gmail.com>
2022-09-16 20:39:54 +0200

    feat: add draft to find-within-array solution

---
+++ find-within-array/find-within-array.spec.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+
+// describe('Get the Middle Character, 7kyu, https://www.codewars.com/kata/get-the-middle-character', () => {
+//   it(`simple case`, () => {
+//     const result = getMiddle('a');
+//     expect(result).to.be.eq('a');
+//   })
+// })
---
+++ find-within-array/find-within-array.ts
@@ -0,0 +1,9 @@
+export function trueIfEven(v: number, i: number) { return v % 2 === 0; }
+export function neverTrue(v: number, i: number) { return false; }
+export function trueIfValueEqualsIndex(v: number, i: number) { return v === i; }
+export function trueIfLengthEqualsIndex(v: string, i: number) { return v.length === i; }
+
+
+// var findInArray = function (array: number[] | string[], iterator: (v: number | string, i: number)) {
+
+// };
