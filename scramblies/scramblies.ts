e461c7630d70475bc684a7ff2d36db0d1cb6c4b5
Yulia Lepner <ylepner@gmail.com>
2022-09-17 18:41:39 +0200

    feat: update solution in scramblies task

--- scramblies/scramblies.ts
+++ scramblies/scramblies.ts
@@ -1,28 +1,22 @@
 export function scramble(str1: string, str2: string) {
-  const str1ToArr = str1.split('')
-  const str2ToArr = str2.split('')
-  const shortArr = defineWhichIsShorter(str1ToArr, str2ToArr)
-  const longArr = defineWhichIsLonger(str1ToArr, str2ToArr)
-  for (let i = 0; i < shortArr.length; i++) {
-    if (longArr.includes(shortArr[i])) {
-      longArr[longArr.indexOf(shortArr[i])] = ''
+  const letters: Record<string, number> = {}
+  for (let letter of str1) {
+    if (letters[letter]) {
+      letters[letter] += 1
+    } else {
+      letters[letter] = 1
+    }
+  }
+  for (let letter of str2) {
+    if (letters[letter]) {
+      if (letters[letter] > 0) {
+        letters[letter] -= 1
+      } else {
+        return false
+      }
     } else {
       return false
     }
   }
   return true
-}
-
-function defineWhichIsShorter(arr1: string[], arr2: string[]) {
-  if (arr1.length <= arr2.length) {
-    return arr1
-  }
-  return arr2
-}
-
-function defineWhichIsLonger(arr1: string[], arr2: string[]) {
-  if (arr1.length > arr2.length) {
-    return arr1
-  }
-  return arr2
 }
