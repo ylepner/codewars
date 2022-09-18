97febfdd1106583e708c72ee2d34444ad0b8f121
Yulia Lepner <ylepner@gmail.com>
2022-09-18 22:03:16 +0200

    feat: complete task find-within-array

--- find-within-array/find-within-array.spec.ts
+++ find-within-array/find-within-array.spec.ts
@@ -1,8 +1,33 @@
 import { expect } from 'chai';
+import { findInArray, neverTrue, trueIfEven, trueIfLengthEqualsIndex, trueIfValueEqualsIndex } from './find-within-array';
 
-// describe('Get the Middle Character, 7kyu, https://www.codewars.com/kata/get-the-middle-character', () => {
-//   it(`simple case`, () => {
-//     const result = getMiddle('a');
-//     expect(result).to.be.eq('a');
-//   })
-// })
+describe('Get the Middle Character, 7kyu, https://www.codewars.com/kata/get-the-middle-character', () => {
+  it(`case 1`, () => {
+    const result = findInArray([], trueIfEven);
+    expect(result).to.be.eq(-1);
+  })
+  it(`case 2`, () => {
+    const result = findInArray([1, 3, 5, 6, 7], trueIfEven);
+    expect(result).to.be.eq(3);
+  })
+  it(`case 3`, () => {
+    const result = findInArray([2, 4, 6, 8], trueIfEven);
+    expect(result).to.be.eq(0);
+  })
+  it(`case 4`, () => {
+    const result = findInArray([2, 4, 6, 8], neverTrue);
+    expect(result).to.be.eq(-1);
+  })
+  it(`case 5`, () => {
+    const result = findInArray([13, 5, 3, 1, 4, 5], trueIfValueEqualsIndex);
+    expect(result).to.be.eq(4);
+  })
+  it(`case 6`, () => {
+    const result = findInArray(["one", "two", "three", "four", "five", "six"], trueIfLengthEqualsIndex);
+    expect(result).to.be.eq(4);
+  })
+  it(`case 7`, () => {
+    const result = findInArray(["bc", "af", "d", "e"], trueIfLengthEqualsIndex);
+    expect(result).to.be.eq(-1);
+  })
+})
--- find-within-array/find-within-array.ts
+++ find-within-array/find-within-array.ts
@@ -4,6 +4,11 @@ export function trueIfValueEqualsIndex(v: number, i: number) { return v === i; }
 export function trueIfLengthEqualsIndex(v: string, i: number) { return v.length === i; }
 
 
-// var findInArray = function (array: number[] | string[], iterator: (v: number | string, i: number)) {
-
-// };
+export const findInArray = function (array: number[] | string[], iterator: (v: number | string, i: number) => boolean) {
+  for (let i = 0; i < array.length; i++) {
+    if (iterator(array[i], i)) {
+      return i
+    }
+  }
+  return -1
+};
