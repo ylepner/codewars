cd720a38271b881ababeb4dc4bd200944f9e982f
Yulia Lepner <ylepner@gmail.com>
2022-09-28 18:57:28 +0200

    fix: correct solution

--- SMS-Shortener/SMS-Shortener.spec.ts
+++ SMS-Shortener/SMS-Shortener.spec.ts
@@ -18,4 +18,8 @@ describe('SMS-Shortener, 6kyu, https://www.codewars.com/kata/535a69fb36973f2aad0
     const result = shortener('SMS messages are limited to 160 characters. It tends to be irritating, especially when freshly written message is 164 characters long. SMS messages are limited to 160 characters. It tends to be irritating, especially when freshly written message is 164 characters long.');
     expect(result).to.be.eq('SMSMessagesAreLimitedTo160Characters.ItTendsToBeIrritating,EspeciallyWhenFreshlyWrittenMessageIs164CharactersLong.SMSMessagesAreLimitedTo160Characters.ItTendsToBeIrritating,EspeciallyWhenFreshlyWrittenMessageIs164CharactersLong.');
   });
+  it('case 5', () => {
+    const result = shortener('ddBRtqaw,o ztSBaIUOg!zgFp ijilZTjQ efh,?uZ IzCQSDfun VbTdXPev?vSZ.e LKN!OGIHuYpkJ .XU.zei JNZ.VER?FzsizK xZf AGy h.zyF Q,uDQA VAOM,gJ,O gT,?AoaMQcd Ghe.Kxm!g!Lpz ccYSXe sPdbxuIq? k,woHhMCT.U DOVEvBhaWn drCRQRD cOAWNvK. !KH.,h Yj!!n,R?Ufoi!h ?XeZyQ!gRRs .lhvusUD,BLill imLEiwWFKuOq T?IfUp F!jqSYSVJbePw aEu.YRhBZ.a ByTUmv .tf.UxLkiUt B?qIJC!z!QzF sQP evWsgTiyPqGT kDFzYlULKfzlm, iMPvP MLRoFm.cPLbwWE AGU!! QWrWWGfB!,uq eKyJrNWxEJSIsD Cc?udieTS');
+    expect(result).to.be.eq('ddBRtqaw,oZtSBaIUOg!zgFpIjilZTjQEfh,?uZIzCQSDfunVbTdXPev?vSZ.eLKN!OGIHuYpkJ.XU.zeiJNZ.VER?FzsizKXZfAGyH.zyFQ,uDQAVAOM,gJ,OGT,?AoaMQcdGhe.Kxm!g!LpzCcYSXeSPdbxuIq?K,woHhMCT.UDOVEvBhaWnDrCRQRDCOAWNvK.!KH.,hYj!!n,R?Ufoi!h?XeZyQ!gRRs.lhvusUD,BLillImLEiwWFKuOqT?IfUpF!jqSYSVJbePwAEu.YRhBZ.aByTUmv.tf.UxLkiUtB?qIJC!z!QzFSQPEvWsgTiyPqGTKDFzYlULKfzlm,IMPvPMLRoFm.cPLbwWEAGU!!QWrWWGfB!,uqEKyJrNWxEJSIsDCc?udieTS');
+  });
 })
--- SMS-Shortener/SMS-Shortener.ts
+++ SMS-Shortener/SMS-Shortener.ts
@@ -9,13 +9,16 @@ export function shortener(message: string): string {
   const sliceArr = messageArrRev.slice(0, diff)
   for (let i = 0; i < diff; i++) {
     if (!sliceArr[i]) {
-      return newArr.reverse().join('')
+      let res = newArr.reverse().join('')
+      res = res.replace(res[0], message[0])
+      return res
     }
     const toUpperCase = sliceArr[i][0].toUpperCase() + sliceArr[i].slice(1)
     newArr.push(toUpperCase)
   }
   const str = newArr.reverse().join('')
   const arrSlice = messageArr.slice(0, messageArr.length - diff)
-  const result = arrSlice.join(' ') + str
+  let result = arrSlice.join(' ') + str
+  result = result.replace(result[0], message[0])
   return result
 }
