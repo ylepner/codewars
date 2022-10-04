c2a4d825297ea6f273da46ca22723026bea11a3f
Yulia Lepner <ylepner@gmail.com>
2022-10-04 20:21:08 +0200

    feat: complete morse-decoder task

---
+++ morse-decoder/morse-decoder.ts
@@ -0,0 +1,69 @@
+export function decode(expr: string) {
+  const words = expr.split('**********')
+  let morse = ''
+  let letters = []
+  words.forEach((word) => {
+    morse += ' '
+    letters = []
+    for (let i = 0; i < word.length; i += 10) {
+      letters.push(Number(word.slice(i, i + 10)))
+    }
+    letters.forEach((letter) => {
+      const letterStr = String(letter)
+      let morseChar: string = ''
+      for (let i = 0; i < letterStr.length; i += 2) {
+        const charCode = letterStr.slice(i, i + 2)
+        morseChar += getMorseChar(charCode)
+      }
+      morse += MORSE_TABLE[morseChar]
+    })
+  });
+  return morse.slice(1)
+}
+
+function getMorseChar(charNumber: string) {
+  if (charNumber === '10') {
+    return '.'
+  } else {
+    return '-'
+  }
+}
+
+const MORSE_TABLE: Record<string, string> = {
+  '.-': 'a',
+  '-...': 'b',
+  '-.-.': 'c',
+  '-..': 'd',
+  '.': 'e',
+  '..-.': 'f',
+  '--.': 'g',
+  '....': 'h',
+  '..': 'i',
+  '.---': 'j',
+  '-.-': 'k',
+  '.-..': 'l',
+  '--': 'm',
+  '-.': 'n',
+  '---': 'o',
+  '.--.': 'p',
+  '--.-': 'q',
+  '.-.': 'r',
+  '...': 's',
+  '-': 't',
+  '..-': 'u',
+  '...-': 'v',
+  '.--': 'w',
+  '-..-': 'x',
+  '-.--': 'y',
+  '--..': 'z',
+  '.----': '1',
+  '..---': '2',
+  '...--': '3',
+  '....-': '4',
+  '.....': '5',
+  '-....': '6',
+  '--...': '7',
+  '---..': '8',
+  '----.': '9',
+  '-----': '0',
+};
