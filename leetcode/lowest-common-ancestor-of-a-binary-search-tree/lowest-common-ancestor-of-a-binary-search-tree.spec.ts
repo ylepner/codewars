c89759af55a300b8346a982354157698fa938389
Yulia Lepner <ylepner@gmail.com>
2023-05-19 11:57:46 +0200

    feat: complete task lowest-common-ancestor

---
+++ leetcode/lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.spec.ts
@@ -0,0 +1,22 @@
+import { expect } from "chai";
+import { lowestCommonAncestor } from "./lowest-common-ancestor-of-a-binary-search-tree";
+
+describe('Lowest Common Ancestor of a Binary Search Tree, Easy, https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/', () => {
+  it('case 1', () => {
+    const tree = new TreeNode(6, new TreeNode(2, new TreeNode(0), new TreeNode(4, new TreeNode(3, new TreeNode(5)))), new TreeNode(8, new TreeNode(7), new TreeNode(9)))
+    const q = new TreeNode(2, new TreeNode(0), new TreeNode(4, new TreeNode(3, new TreeNode(5))))
+    const p = new TreeNode(8, new TreeNode(7), new TreeNode(9))
+    expect(lowestCommonAncestor(tree, q, p)).to.be.eq(tree)
+  })
+})
+
+export class TreeNode {
+  val: number
+  left: TreeNode | null
+  right: TreeNode | null
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+  }
+}
---
+++ leetcode/lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.ts
@@ -0,0 +1,19 @@
+import { TreeNode } from "./lowest-common-ancestor-of-a-binary-search-tree.spec";
+
+export function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
+  if (root === null) {
+    return null
+  }
+  if (p !== null && q !== null) {
+    if (p.val < root.val && q.val < root.val) {
+      return lowestCommonAncestor(root.left, p, q)
+    }
+    else if (p.val > root.val && q.val > root.val) {
+      return lowestCommonAncestor(root.right, p, q)
+    }
+    else {
+      return root
+    }
+  }
+  return root
+};
