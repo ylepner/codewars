a63c1c71c902794fb9504b1e1c924b64607c157e
Yulia Lepner <ylepner@gmail.com>
2023-05-22 11:36:49 +0200

    feat: implement function createBST

--- leetcode/lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.spec.ts
+++ leetcode/lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.spec.ts
@@ -1,17 +1,18 @@
 import { expect } from "chai";
 import { lowestCommonAncestor, lowestCommonAncestor2 } from "./lowest-common-ancestor-of-a-binary-search-tree";
+import { createBST } from "../../helpful-functions/create-binary-search-tree/create-binary-search-tree";
 
 describe('Lowest Common Ancestor of a Binary Search Tree, Easy, https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/', () => {
   it('case 1', () => {
-    const tree = new TreeNode(6, new TreeNode(2, new TreeNode(0), new TreeNode(4, new TreeNode(3, new TreeNode(5)))), new TreeNode(8, new TreeNode(7), new TreeNode(9)))
-    const q = new TreeNode(2, new TreeNode(0), new TreeNode(4, new TreeNode(3, new TreeNode(5))))
-    const p = new TreeNode(8, new TreeNode(7), new TreeNode(9))
+    const tree = createBST([6, 2, 8, 0, 4, 7, 9, 3, 5])
+    const q = createBST([2, 0, 4, 3, 5])
+    const p = createBST([8, 7, 9])
     expect(lowestCommonAncestor(tree, q, p)).to.be.eq(tree)
   })
   it('case 2 non-recursive', () => {
-    const tree = new TreeNode(6, new TreeNode(2, new TreeNode(0), new TreeNode(4, new TreeNode(3, new TreeNode(5)))), new TreeNode(8, new TreeNode(7), new TreeNode(9)))
-    const q = new TreeNode(2, new TreeNode(0), new TreeNode(4, new TreeNode(3, new TreeNode(5))))
-    const p = new TreeNode(8, new TreeNode(7), new TreeNode(9))
+    const tree = createBST([6, 2, 8, 0, 4, 7, 9, 3, 5])
+    const q = createBST([2, 0, 4, 3, 5])
+    const p = createBST([8, 7, 9])
     expect(lowestCommonAncestor2(tree, q, p)).to.be.eq(tree)
   })
 })
